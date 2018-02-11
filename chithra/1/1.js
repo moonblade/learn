bdd305d2b80bf12c65625e50f5f0f3189f3fd064
chithrasasidharan <chithratheertham@gmail.com>
2018-02-11 11:04:29 +0530

    l

--- chithra/1/1.js
+++ chithra/1/1.js
@@ -1,8 +1,11 @@
-function myFunction() {
+function clickMe() {
 	if(document.getElementById('t1').value=="")
 		alert("give a valid input");
 	else
     	alert(document.getElementById('t1').value);
     document.getElementById("t1").value = "";
 
+}
+function clickOkay(){
+	alert("Welcome");
 }
