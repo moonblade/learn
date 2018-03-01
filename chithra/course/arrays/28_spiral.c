cac5ef604d1a8be8feb7e1c5ad6782d519ae93fc
moonblade <moonblade168@gmail.com>
2018-03-01 21:57:35 +0530

    changes

--- chithra/course/arrays/28_spiral.c
+++ chithra/course/arrays/28_spiral.c
@@ -6,23 +6,27 @@
 
 void printSpiral(int a[][N]){
 	for(int k=0;k<=R/2;k++){
-		for(int j=k; j<N-k; j++)
-			printf("%d ",a[k][j] );
-		for(int i=k+1;i<R-1-k;i++)
-			printf("%d ",a[i][N-1-k] );
-		if (k!=R/2 && R%2!=0){	
+		if (k<R/2 || (R-2-k==R/2 && N%2!=0)){			
+			for(int j=k; j<N-k; j++)
+				printf("%d ",a[k][j] );
+			for(int i=k+1;i<R-1-k;i++)
+				printf("%d ",a[i][N-1-k] );
 			for(int j=N-1-k;j>=k;j--)
 				printf("%d ",a[R-1-k][j]);
-		}
 			for(int i=R-2-k;i>k;i--)
 				printf("%d ",a[i][k]);
+		}
 	}
 	printf("\n");
 }
 
 int main(int argc, char const *argv[])
 {
-	int a[][N] = {{1,2,3,4,5},{7,8,9,10,11},{13,14,15,16,17},{18,19,20,21,22}};
+	int a[][N] = {
+		{1,2,3,4,5},
+		{7,8,9,10,11},
+		{13,14,15,16,17},
+		{18,19,20,21,22}};
 	/*
 		1  2  3  4  5  6
 		7  8  9 10 11 12
