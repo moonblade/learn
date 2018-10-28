31add4792dcb47c31b8d49d584e07777e2da071d
Mohammed Nisham <moonblade@Mohammeds-MacBook-Air.local>
2018-10-28 15:21:55 +0530

    remove items that sum upto zero in a linked list

--- sum.py
+++ sum.py
@@ -15,10 +15,35 @@ class node:
         if self.link is not None:
             self.link.printAll()
 
-a=node(-10)
+a=node(20)
+a=node(-10,a)
 a=node(8, a)
 a=node(-5, a)
 a=node(-3, a)
 head=node(10, a)
 
+head.printAll()
+
+def removeZero(head):
+    s = head
+    while(s is not None and s.link is not None):
+        e = s
+        sum = 0
+        zerosum=False
+        while (e is not None and e.link is not None):
+            sum+=e.num
+            if sum==0:
+                s.link = e.link
+                if (s==head):
+                    head = e.link
+                zerosum = True
+                break
+            else:
+                e=e.link
+        if (not zerosum):
+            s=s.link
+    return head
+        
+print("\nremoved")
+head=removeZero(head)
 head.printAll()
